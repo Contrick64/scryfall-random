e98a88a1dd37c09bac0b7f460759d7a6502193b5
Connor <connor@murphysites.com>
2023-01-27 13:45:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Container } from "react-bootstrap";
+import CardPreview from "./CardPreview";
+
+export default function App(props) {
+  return (
+    <Container>
+      <CardPreview />
+    </Container>
+  );
+}
