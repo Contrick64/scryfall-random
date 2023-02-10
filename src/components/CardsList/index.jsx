54b4115696a599afdb1d19dcf2d1c1b5f86b6e06
Connor <connor@murphysites.com>
2023-02-09 19:39:45 -0500

    add cardslist animation

--- src/components/CardsList/index.jsx
+++ src/components/CardsList/index.jsx
@@ -1,24 +1,36 @@
 import "./index.scss";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { createRef } from "react";
 
 export default function CardsList({ card, cards, setCard, ...attrs }) {
-  const cardElements = cards.map((cardObj, i) => {
+  const cardRef = cards.map((cardObj) => {
+    return { ...cardObj, nodeRef: createRef(null) };
+  });
+  const cardElements = cardRef.map((cardObj, i) => {
     const imgSrc = (cardObj.card_faces ? cardObj.card_faces[0] : cardObj)
-      .image_uris.png;
+      .image_uris.small;
     return (
-      <button
+      <CSSTransition
         key={cardObj.id}
-        className={`cardslist-card unbutton ${i == card ? "selected" : ""}`}
-        onClick={() => setCard(i)}
+        classNames="transition-card"
+        timeout={600}
       >
-        <img src={imgSrc} />
-      </button>
+        <button
+          className={`cardslist-card unbutton ${i == card ? "selected" : ""}`}
+          onClick={() => setCard(i)}
+        >
+          <img src={imgSrc} />
+        </button>
+      </CSSTransition>
     );
   });
   return (
-    <footer {...attrs} className={`card-row mt-auto footer ${attrs.className}`}>
-      <TransitionGroup>
-        {cardElements ? cardElements : "No card history to show"}
-      </TransitionGroup>
-    </footer>
+    <TransitionGroup
+      {...attrs}
+      className={`card-row mt-auto footer ${attrs.className}`}
+      component="footer"
+    >
+      {cardElements ? cardElements : "No card history to show"}
+    </TransitionGroup>
   );
 }
